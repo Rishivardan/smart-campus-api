f914c3b7f4aea8c824f6ab4292b805cadd8b4d33
V.Rishivardan <rishivardan2004@gmail.com>
2026-04-20 13:15:36 +0530

    Main.java file changes made

--- src/main/java/com/example/smartcampus/Main.java
+++ src/main/java/com/example/smartcampus/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) {
         HttpServer server = startServer();
-        System.out.println("Smart Campus API running at " + BASE_URI + "api/v1");
+        System.out.println("Smart Campus API running at " + BASE_URI);
         Runtime.getRuntime().addShutdownHook(new Thread(server::shutdownNow));
     }
 }
